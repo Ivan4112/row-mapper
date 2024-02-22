90238a9d8233b78fed5b2f20dee148d833a0b277
Ivan4112 <ivanbondar224@gmail.com>
2024-02-22 11:09:28 +0200

    change Factory class

--- src/main/java/com/epam/rd/autocode/RowMapperFactory.java
+++ src/main/java/com/epam/rd/autocode/RowMapperFactory.java
@@ -1,40 +1,10 @@
 package com.epam.rd.autocode;
 
 import com.epam.rd.autocode.domain.Employee;
-import com.epam.rd.autocode.domain.FullName;
-import com.epam.rd.autocode.domain.Position;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
-
-public class RowMapperFactory implements RowMapper<Employee>{
-
+public class RowMapperFactory {
 
     public RowMapper<Employee> employeeRowMapper() {
-        return this;
-    }
-
-    @Override
-    public Employee mapRow(ResultSet resultSet) {
-        Employee employee = null;
-        try {
-            String id = String.valueOf(resultSet.getLong("ID"));
-            String firstName = resultSet.getString("FIRSTNAME");
-            String lastName = resultSet.getString("LASTNAME");
-            String middleName = resultSet.getString("MIDDLENAME");
-            Position position = Position.valueOf(resultSet.getString("POSITION"));
-            LocalDate localDate = resultSet.getObject("HIREDATE", LocalDate.class);
-            String salary = resultSet.getString("SALARY");
-
-            employee = new Employee(new BigInteger(id),
-                    new FullName(firstName, lastName, middleName),
-                    position, localDate, new BigDecimal(salary));
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-        return employee;
+        return new RowMapperImpl();
     }
 }
